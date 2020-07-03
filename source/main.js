214b365dc43ce0edd0876ce0e85f690fe0904258
kazz <kazzzz1001@gmail.com>
2020-07-04 04:06:18 +0900

    不要なif削除

--- source/main.js
+++ source/main.js
@@ -1,14 +1,12 @@
-if (location.pathname === "/search/") {
-  const main_h2_top = document.querySelector(".main_h2").getBoundingClientRect()
-    .top;
-  scrollTo(0, main_h2_top);
+const main_h2_top = document.querySelector(".main_h2").getBoundingClientRect()
+  .top;
+scrollTo(0, main_h2_top);
 
-  addEventListener("keydown", (e) => {
-    if (e.keyCode === 39) {
-      document.querySelector(".to_next a").click();
-    }
-    if (e.keyCode === 37) {
-      document.querySelector(".to_prev a").click();
-    }
-  });
-}
+addEventListener("keydown", (e) => {
+  if (e.keyCode === 39) {
+    document.querySelector(".to_next a").click();
+  }
+  if (e.keyCode === 37) {
+    document.querySelector(".to_prev a").click();
+  }
+});
