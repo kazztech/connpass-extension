9a7ab3dead9db3e05053305a60316c3c9b509d67
kazz <kazzzz1001@gmail.com>
2020-07-04 14:39:29 +0900

    nextとprevにifを噛ませた

--- source/main.js
+++ source/main.js
@@ -1,12 +1,17 @@
-const main_h2_top = document.querySelector(".main_h2").getBoundingClientRect()
-  .top;
+const mainH2Top = document.querySelector(".main_h2").getBoundingClientRect().top;
 scrollTo(0, main_h2_top);
 
 addEventListener("keydown", (e) => {
   if (e.keyCode === 39) {
-    document.querySelector(".to_next a").click();
+    const nextLink = document.querySelector(".to_next a");
+    if (nextLink) {
+      nextLink.click();
+    }
   }
   if (e.keyCode === 37) {
-    document.querySelector(".to_prev a").click();
+    const prevLink = document.querySelector(".to_prev a");
+    if (prevLink) {
+      prevLink.click();
+    }
   }
 });
